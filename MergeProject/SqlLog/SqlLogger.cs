a06b19ce12658e142b356b35488d6d883a8fdda6
Jan <jan.behro@gmail.com>
2019-03-13 16:23:17 +0100

    Sql logger

---
+++ MergeProject/SqlLog/SqlLogger.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlLog
+{
+    public class SqlLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
